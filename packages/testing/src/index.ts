da5778a0712ab4b3536b1fe2bd316142377e02c5
Li41 <li41qq@gmail.com>
2026-08-16 14:06:47 +0800

    export in-memory question import repository

--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./fake-item-repository.js";
 export * from "./fake-question-bank-repository.js";
+export * from "./fake-question-import-repository.js";
 export * from "./fake-question-structure-repository.js";
