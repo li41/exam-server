dc731b50c6aae064f0a9e6e523a370b60e471736
Li41 <li41qq@gmail.com>
2026-08-16 16:51:31 +0800

    export in-memory test booklet repository

--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -2,3 +2,4 @@ export * from "./fake-item-repository.js";
 export * from "./fake-question-bank-repository.js";
 export * from "./fake-question-import-repository.js";
 export * from "./fake-question-structure-repository.js";
+export * from "./fake-test-booklet-repository.js";
