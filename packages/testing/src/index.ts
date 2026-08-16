c9c3cc4354b9b215bac374cd76c96f58b08e16c3
Li41 <li41qq@gmail.com>
2026-08-16 18:34:02 +0800

    export examinee fake repository

--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -3,3 +3,4 @@ export * from "./fake-question-bank-repository.js";
 export * from "./fake-question-import-repository.js";
 export * from "./fake-question-structure-repository.js";
 export * from "./fake-test-booklet-repository.js";
+export * from "./fake-examinee-repository.js";
