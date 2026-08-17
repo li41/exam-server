26663f2d4bfbba0f69d3e94b2beb4f3f2d28051d
Li41 <li41qq@gmail.com>
2026-08-17 15:22:52 +0800

    export affair B-wave fake adapter

--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -5,3 +5,4 @@ export * from "./fake-question-structure-repository.js";
 export * from "./fake-test-booklet-repository.js";
 export * from "./fake-examinee-repository.js";
 export * from "./fake-affair-repository.js";
+export * from "./fake-affair-configuration-repository.js";
